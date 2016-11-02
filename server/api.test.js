221ad46939f00fd53318c6356a1c62f5f9cba2a1
Shane Jarman <sjarman91@gmail.com>
2016-11-01 20:25:36 -0400

    artist tests partially complete

--- server/api.test.js
+++ server/api.test.js
@@ -2,7 +2,18 @@ const request = require('supertest-as-promised')
 const {expect} = require('chai')
 const db = require('APP/db')
 const User = require('APP/db/models/user')
+const Artist = require('APP/db/models/artist')
 const app = require('./start')
+
+
+// AN AUTHORIZED USER FROM AUTH TESTS
+
+const alice = {
+  username: 'alice@secrets.org',
+  password: '12345'
+}
+
+// USER API TESTS ----------------------------------------------
 
 describe('/api/users', () => {
   describe('when not logged in', () => {
@@ -10,7 +21,7 @@ describe('/api/users', () => {
       request(app)
         .get(`/api/users/1`)
         .expect(401)
-    )    
+    )
 
     it('POST creates a user', () =>
       request(app)
@@ -32,7 +43,67 @@ describe('/api/users', () => {
         .redirects(1)
         .then(res => expect(res.body).to.contain({
           email: 'eve@interloper.com'
-        }))        
+        }))
     )
   })
 })
+
+// ARTIST API TESTS ----------------------------------------------
+const jackson = {name: 'dj jackson', bio: 'im cool'}
+const hannah = {name: 'dj hannah', bio: 'im chill'}
+const andrew = {name: 'dj andrew', bio: 'im here to drop fire beats'}
+
+describe('/api/artists', () => {
+  before('creates two artists', () =>
+    db.didSync
+      .then(() =>
+        Artist.bulkCreate([jackson, hannah])
+      )
+  )
+
+// tests for guests
+  describe('when not logged in', () => {
+    describe('sends all artists', () => {
+      it('is proper length', () =>
+        request(app).get('/api/artists')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.have.length.of(2)
+          })
+      )
+    })
+
+    describe('sends one artist', () => {
+      it('sends the correct artist', () =>
+        request(app).get('/api/artists/1')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.contain(jackson)
+          })
+      )
+    })
+
+    describe('fails to delete an artist', () => {
+      it('responds with not authorized', () =>
+        request(app)
+          .delete('/api/artists/1')
+          .expect(401)
+      )
+    })
+
+  })
+
+// tests for admins
+  describe('when logged in as admin', () => {
+    // describe('delete one artist', () => {
+    //   it('is proper length', () =>
+    //     request(app)
+    //       .delete('/api/artists/3')
+    //       .expect(204)
+    //   )
+    // })
+  })
+})
+
+
+// API TESTS ----------
---
+++ server/artists.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const epilogue = require('./epilogue')
+const db = require('APP/db')
+
+const customArtistRoutes = require('express').Router()
+
+// Custom routes go here.
+
+module.exports = customArtistRoutes
+
+// Epilogue will automatically create standard RESTful routes
+const artists = epilogue.resource({
+  model: db.model('artists'),
+  endpoints: ['/artists', '/artists/:id']
+})
+
+const {mustBeLoggedIn, selfOnly, forbidden} = epilogue.filters
+artists.delete.auth(mustBeLoggedIn)
+artists.delete.auth(selfOnly)
