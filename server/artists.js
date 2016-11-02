d38490b087e74afd1293d191df6ac5b2b4ec9623
Colten Jackson <Colten Jackson>
2016-11-02 12:56:20 -0400

    Added a little commentary for epilogue

--- server/artists.js
+++ server/artists.js
@@ -15,5 +15,10 @@ const artists = epilogue.resource({
   endpoints: ['/artists', '/artists/:id']
 })
 
+//Artist API simply instantiates a Router object, and exports it
+//but it also imports epilogue, have it create default routes
+//destructure filters object back into functions which either CONTINUE or STOP, and in STOP cases respond with 403, etc.
 const {mustBeLoggedIn, selfOnly, forbidden, mustBeAdmin} = epilogue.filters
+//epilogue filters are tests that have to pass (CONTINUE) in order for the api route to succeed
 artists.delete.auth(mustBeAdmin)
+
--- server/epilogue.js
+++ server/epilogue.js
@@ -14,7 +14,7 @@ const mustBeLoggedIn = (req, res, context) => {
   return context.continue
 }
 
-
+//mustBeAdmin is a function that we export so we can require epilogue.js
 const mustBeAdmin = (req, res, context) => {
   if (!req.user) {
     res.status(401).send('You must be logged in')
@@ -41,6 +41,6 @@ const forbidden = message => (req, res, context) => {
   res.status(403).send(message)
   return context.stop
 }
-
+//filter functions are properties on an object which is set to the filter property of EPILOQUE, which we then export
 epilogue.filters = {mustBeLoggedIn, selfOnly, forbidden,mustBeAdmin}
 module.exports = epilogue
