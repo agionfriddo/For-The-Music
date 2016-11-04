bef3ae5fac7a850c08e156566c53f69950dfccd6
Andrew Gionfriddo <andrew.gionfriddo@gmail.com>
2016-11-04 13:36:37 -0400

    search bar has button

--- app/components/SearchBar.jsx
+++ app/components/SearchBar.jsx
@@ -7,10 +7,13 @@ class SearchBar extends Component {
 
   render() {
     return (
-        <div className="control-group" >
-          <div className="form-group">
-            <label>Search:</label>
+        <div className="form-group" >
+          <label>Search:</label>
+          <div className="input-group">
             <input type="text" className="form-control" />
+            <span className="input-group-btn">
+              <button className="btn btn-success">Go!</button>
+            </span>
           </div>
         </div>
     )
