e65d2593d6c55acbf49e1bf96421c6710b72acbb
Colten Jackson <jazzyjackson@gmail.com>
2016-11-04 13:36:13 -0400

    regexes the price and doesnt hit ticket route with undefined

--- app/components/cart-item.jsx
+++ app/components/cart-item.jsx
@@ -8,7 +8,7 @@ export default ({ticket}) => (
       <div className="col-md-6">
         <h6>{ticket.event.artists[0].name} & {ticket.event.artists[1].name} @ {ticket.event.venue.name}</h6>
         <p>Price: ${ticket.event.ticketPrice}</p>
-        <p>Address: ${ticket.event.venue.address}</p>
+        <p>Address: {ticket.event.venue.address}</p>
       </div>
       <div className="col-md-2">
         <p>Delete Ticket</p>
