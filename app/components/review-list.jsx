22b15061f791d61ab3100b912cd9cb6c2d54f8a2
Hannah Cain <hcain23@gmail.com>
2016-11-07 14:56:55 -0500

    fixed db seed file, no more process.exit

---
+++ app/components/review-list.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import ReviewItemComponent from './review-item';
+import { connect } from 'react-redux';
+
+class ReviewList extends Component {
+
+  render () {
+    const { reviewsList } = this.props
+
+    return (
+      <div>
+      <h3>Reviews</h3>
+        <div className="list-group">
+          {
+            reviewsList && reviewsList.map(reviewsList => (
+              <div key={ review. id } className="list-group-item">
+                <ReviewItemComponent review= { review }/>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({reviewsList: state.reviewsList})
+
+export default connect(mapStateToProps)(ReviewList)
---
+++ app/reducers/reviews.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+//-------------- CONSTANTS
+const SET_REVIEWS_BY_VENUE = 'SET_REVIEWS_BY_VENUE'
+
+
+//-------------- SYNC ACTION CREATORS
+export 
