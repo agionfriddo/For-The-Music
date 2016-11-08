00375a5e6bbabfb651cd886d4e86aca945dabea7
Colten Jackson <jazzyjackson@gmail.com>
2016-11-07 20:28:00 -0500

    Made some real clever stars there.

--- app/components/review-item.jsx
+++ app/components/review-item.jsx
@@ -7,20 +7,23 @@ export default class ReviewItem extends Component {
   }
 
 	buildStarsBoolean(rating){
-			let booleanStars = [];
-			for(let i = 0; i < 5; i++){
-				booleanStars.push(rating > i);
-			}
-			return booleanStars;
-	}
+		return Array.from(new Array(5), (element, index) => rating > index);
+	} 
 				
   render() {
     const review = this.props.review
-		console.log(buildStarsBoolean(review.rating));
+		console.log(this.buildStarsBoolean(review.rating));
     return (
       <div className="row">
         <div className="col-md-12">
-          <h4>{ review.title } -- { review.rating } Stars</h4>
+          <h4>
+						{
+							this.buildStarsBoolean(review.rating).map(star => {
+								return star ? <span className="glyphicon glyphicon-star"></span> : <span className="glyphicon glyphicon-star-empty"></span>;
+							})
+				 		}
+					</h4>
+					<h4>{ review.title }</h4>
           <p>By {review.user.name }</p>
           <p>{ review.content }</p>
         </div>
